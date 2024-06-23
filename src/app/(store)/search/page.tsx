d759390bd59478c9ed270a4c861b145bf820b7df
RafaelMatos <rafael-matos@outlook.com>
2024-06-23 08:02:33 -0300

    feat: add search products page

---
+++ src/app/(store)/search/page.tsx
@@ -0,0 +1,73 @@
+import Link from 'next/link'
+import { numberToPrice } from '../(home)/page'
+import Image from 'next/image'
+import { redirect } from 'next/navigation'
+import { api } from '@/data/api'
+import { Product } from '@/data/types/product'
+interface SearchProps {
+  searchParams: {
+    q: string
+  }
+}
+
+async function getSearchProducts(query: string): Promise<Product[]> {
+  const response = await api(`/products/search?q=${query}`, {
+    next: {
+      revalidate: 60 * 60,
+    },
+  })
+  const products = await response.json()
+
+  return products
+}
+export default async function Search({ searchParams }: SearchProps) {
+  const { q: query } = searchParams
+  if (!query) {
+    redirect('/')
+  }
+
+  const products = await getSearchProducts(query)
+
+  return (
+    <div className="flex flex-col gap-4">
+      <p>
+        Resultados para: <span className="font-semibold">{searchParams.q}</span>
+      </p>
+
+      <div className="grid grid-cols-3  gap-6">
+        {products.map((product) => {
+          return (
+            <Link
+              key={product.id}
+              href={product.image}
+              className="group relative  rounded-lg bg-zinc-900 overflow-hidden
+        flex justify-center items-start"
+            >
+              <Image
+                src={product.image}
+                className="group-hover:scale-105 transition-transform duration-500"
+                width={400}
+                height={400}
+                quality={100}
+                alt={product.title}
+              />
+              <div
+                className="absolute bottom-10 right-10 h-12
+            flex items-center gap-2 max-w-[288px] rounded-full border-2 
+            border-zinc-500 bg-black/60 p-1 pl-5"
+              >
+                <span className="text-sm truncate">{product.title}</span>
+                <span
+                  className="flex h-full items-center justify-center
+          rounded-full bg-violet-500 px-4 font-semibold"
+                >
+                  {numberToPrice(product.price)}
+                </span>
+              </div>
+            </Link>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
