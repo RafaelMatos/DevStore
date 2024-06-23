ba3285b57b32917b4b51d2908acc251f6f8907dc
RafaelMatos <rafael-matos@outlook.com>
2024-06-23 07:58:47 -0300

    feat: add search products api route

---
+++ src/app/api/products/search/route.ts
@@ -0,0 +1,20 @@
+import type { NextRequest } from 'next/server'
+import data from '../data.json'
+import { z } from 'zod'
+
+export async function GET(request: NextRequest) {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
+  const { searchParams } = request.nextUrl
+
+  const query = z.string().parse(searchParams.get('q'))
+
+  const products = data.products.filter((product) => {
+    return product.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+  })
+
+  if (!products) {
+    return Response.json({ message: 'Product not found.' }, { status: 400 })
+  }
+  return Response.json(products)
+}
